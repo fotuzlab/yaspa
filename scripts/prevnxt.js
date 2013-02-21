291071ceae19c1bd74c79bc6366b0548d97d0be0
fotuzlab <arijitdutta@fotuzlab.in>
2013-02-21 16:43:23 +0530

    added support for swipe

--- scripts/prevnxt.js
+++ scripts/prevnxt.js
@@ -33,3 +33,30 @@ yaspaApp.directive('prevNxtKeys', function($location, $http){
     });
   }	
 });
+
+yaspaApp.directive('prevNxtGesture', function($location, $http){
+  return function(scope, element){
+  	var loc = $location.path();
+    element.bind('swipeleft', function(e){
+    	yaspaswipe('prev', loc, scope, $http, $location);
+    });
+    element.bind('swiperight', function(e){
+    	yaspaswipe('nxt', loc, scope, $http, $location);
+    });
+  }	
+});
+
+var yaspaswipe = function(dir, loc, scope, $http, $location) {console.log(dir);
+	$http.get("data/nav.json").success(function(data){
+      	  for(var i=0; i < data.length; i++){
+            if(data[i].id == loc){
+              if(dir=='nxt' && data[i+1]){ //next
+                $location.path(data[i+1].id);
+              }
+              else if(dir=='prev' && data[i-1]){ //prev
+                $location.path(data[i-1].id);
+              }
+            }
+      }
+    });
+}
