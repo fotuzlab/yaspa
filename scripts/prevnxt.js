3e5927ee665ceabebcadadecbf9dbdf9776914db
fotuzlab <arijitdutta@fotuzlab.in>
2013-02-21 22:06:01 +0530

    updated nav tags

--- scripts/prevnxt.js
+++ scripts/prevnxt.js
@@ -4,7 +4,7 @@
 
 yaspaApp.directive('prevNxt', function(){
   return function(){
-    $('.nav li a').bind('click', function() {
+    $('.nav-collapse li a').bind('click', function() {
     if($('.btn-navbar').is(":visible")) {
       $('.btn-navbar').click();
     }
