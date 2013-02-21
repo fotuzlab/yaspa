d4f91fd66ef0d5ffbf648236ef00f88dda8c867f
fotuzlab <arijitdutta@fotuzlab.in>
2013-02-21 19:30:22 +0530

    updated code. did a reset and added chhanges again to resolve a bug

--- scripts/prevnxt.js
+++ scripts/prevnxt.js
@@ -33,3 +33,37 @@ yaspaApp.directive('prevNxtKeys', function($location, $http){
     });
   }	
 });
+
+yaspaApp.directive('prevNxtGesture', function($location, $http){
+  return function(scope, element){
+  	var loc = $location.path();
+    element.bind('swipeone', function(event_, obj){console.log(obj.description);
+    	switch(obj.description){
+    		case 'swipe:1:right:up':
+    		case 'swipe:1:right:down':
+    		case 'swipe:1:right:steady':
+    		  yaspaswipe('nxt', loc, scope, $http, $location);
+    		break;
+    		case 'swipe:1:left:up':
+    		case 'swipe:1:left:down':
+    		case 'swipe:1:left:steady':
+    		  yaspaswipe('prev', loc, scope, $http, $location);
+    		break;
+    	}
+    	function yaspaswipe(dir, loc, scope, $http, $location) {console.log(dir);
+	$http.get("data/nav.json").success(function(data){
+      	  for(var i=0; i < data.length; i++){
+            if(data[i].id == loc){
+              if(dir=='nxt' && data[i+1]){ //next
+                $location.path(data[i+1].id);
+              }
+              else if(dir=='prev' && data[i-1]){ //prev
+                $location.path(data[i-1].id);
+              }
+            }
+      }
+    });
+}
+    });
+  }	
+});
